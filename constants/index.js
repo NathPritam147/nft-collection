83e9f434df66b85540a69aa9a709079525022abb
Pritam Nath <pnath147@gmail.com>
2022-11-27 21:45:44 +0530

    first commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x8bB9564513088feb1B139D2c145Dd98Ef6B86fef";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
